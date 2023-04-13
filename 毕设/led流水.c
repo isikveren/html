c36612fa2ead32a6e868a9208c0d36d30f7ca3b7
isikveren <lauxunzi@outlook.com>
2023-04-13 21:19:47 +0800

    update

---
+++ 毕设/led流水.c
@@ -0,0 +1,45 @@
+#include "stm32f10x.h" // Device header
+#include "Delay.h"
+
+int main(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // 推挽输出
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; //
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    while (1)
+    {
+        // GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
+        // Delay_ms(500);
+        // GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+        // Delay_ms(500);
+
+        GPIO_Write(GPIOA, ~0x0001);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0002);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0004);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0008);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0010);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0020);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0040);
+        Delay_ms(500);
+        GPIO_Write(GPIOA, ~0x0080);
+        Delay_ms(500);
+
+        // GPIO_Write(GPIOA,~0x0008);
+        // Delay_ms(500);
+        // GPIO_Write(GPIOA,~0x0004);
+        // Delay_ms(500);
+        // GPIO_Write(GPIOA,~0x0002);
+        // Delay_ms(500);
+    }
+}
