51124191377af80b6a1c52620de781cd1e64e1b0
isikveren <lauxunzi@outlook.com>
2023-04-18 15:25:35 +0800

    update

--- 1.c
+++ 1.c
@@ -1,11 +1,36 @@
-#include <stdio.h>
-
-int main()
+if (temper > 24)
 {
-    int a = 1;
-    if (a >= 1)
-    {
-
-        printf("hello");
-    }
+    LED0_ON();
+}
+else
+{
+    LED0_OFF();
+}
+if (KeyNum == 1)
+{
+    Angle += 1;
+    // Buzzer_us(1912,1);
+    Buzzer_Turn();
+    LED1_Turn();
+}
+if (KeyNum == 2)
+{
+    Angle -= 1;
+    // Buzzer_us(1500,1);
+    Buzzer_Turn();
+    LED2_Turn();
+}
+if (KeyNum == 3)
+{
+    Angle += 1;
+    // Buzzer_us(1912,1);
+    Buzzer_Turn();
+    LED1_ON();
+}
+if (KeyNum == 4)
+{
+    Angle -= 1;
+    // Buzzer_us(1500,1);
+    Buzzer_Turn();
+    LED1_OFF();
 }
